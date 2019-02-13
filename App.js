3ce2a895b248ce38e2ff663e11991b7f222fb855
Jeremy Wiebe <jeremy@khanacademy.org>
2019-02-13 14:22:48 -0800

    Initial text rotation bug demo

---
+++ App.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, { Component } from "react";
+import { Dimensions, ScrollView, Text, View } from "react-native";
+
+type Props = {};
+type State = { test: boolean };
+
+export default class App extends Component<Props, State> {
+  state = { test: true };
+
+  dimensionChangedHandler = () => {
+    console.log("Dimension change");
+
+    this.setState({ test: !this.state.test });
+  };
+
+  constructor() {
+    super();
+    Dimensions.addEventListener("change", this.dimensionChangedHandler);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.dimensionChangedHandler);
+  }
+
+  render() {
+    return (
+      <ScrollView
+        contentContainerStyle={{
+          marginTop: 30,
+          marginHorizontal: 16,
+          backgroundColor: "#4a0047"
+        }}
+      >
+        <View
+          style={[
+            {
+              flex: 1,
+              padding: 16,
+              backgroundColor: "#f902ee"
+            }
+            // This forces the overall stylesheet of the view to change after
+            // rotation, which seems to cause RN to properly recalculate this
+            // view's frame.
+            // , this.state.test ? { paddingBottom: 12 } : null
+          ]}
+        >
+          <View style={{ backgroundColor: "#fee5fd" }}>
+            <Text style={{ fontSize: 30 }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
+}
